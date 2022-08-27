d7edbd4b3d79c2fafe1e11e79eb413767b809f06
Ronie Salgado <roniesalg@gmail.com>
2022-08-27 01:01:35 -0400

    Source structure refactoring.

--- sysmelbc.sh
+++ sysmelbc.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 python3 bootstrap/interpreter/sysmelbi.py \
-    -sources module-sources/Compiler/Phase1-Untyped/Sources.lst \
+    -sources module-sources/Compiler/BootstrapUntypedPhase/Sources.lst \
+    -sources module-sources/Compiler/SSA/Sources.lst \
     -sources module-sources/Compiler/Phase2-Typed/Sources.lst \
     -- $@
